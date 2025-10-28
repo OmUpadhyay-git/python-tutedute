4e56ba6410eb01086be6ec7765a7d6472ffc96c1
OmUpadhyay-git <omupadhyay0110@gmail.com>
2025-10-28 21:28:58 +0530

    Flatten Assigement_1 into repository

---
+++ Assigement_1/task1.py
@@ -0,0 +1,13 @@
+input1 = int(input("Enter a first number: "))
+input2 = int(input("Enter a second number: "))
+
+addition = input1 + input2
+subtraction = input1 - input2
+multiplication = input1 * input2
+division = input1 / input2
+
+
+print("Addition:", addition)  
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
